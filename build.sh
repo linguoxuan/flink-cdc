d9ddcebbdabe36b23e6dcf1b20279f78018cd209
jinoran <jinoran@tencent.com>
2025-10-31 16:01:37 +0800

    [mysql|postgres|oracle] Modify build tools.

--- build.sh
+++ build.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
+################################################################################
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
 
 if [ $# -ne 3 ]; then
     echo "Usage: $0 <target_dir> <flink_version> <connector_name>"
-    echo "Example: $0 /tmp 1.16 mysql/flink-sql-connector-mysql-cdc/all"
+    echo "Example: $0 /tmp 1.20 mysql/flink-sql-connector-mysql-cdc/all"
     exit 1
 fi
-ALLOWED_VERSIONS=("1.13" "1.14" "1.16" "1.18")
-FLINK_CDC_VERSION="2.2"
+ALLOWED_VERSIONS=("1.18" "1.20")
+FLINK_CDC_VERSION="3.4"
 OCEANUS_VERSION="9.24"
 TARGET_DIR="$1"
 FLINK_VERSION="$2"
@@ -45,13 +61,6 @@ fi
 mkdir -p "${TARGET_DIR}"
 
 echo "=== Starting project build ==="
-
-git clone https://pacinogong:%mail135246@git.woa.com/InLong/SyncWithGitHub.git
-cd SyncWithGitHub
-git checkout origin/feature-r3
-mvn clean install -DskipTests -Dfast -pl inlong-sort/sort-connectors/base,inlong-sort/sort-formats/format-json -am
-cd ../
-rm -rf SyncWithGitHub
 echo "=== Building connector: $connector ==="
 mvn clean package -DskipTests -pl "$CONNECTOR_NAME" -am -Pflink-${FLINK_VERSION}
 if [ $? -ne 0 ]; then
